3232fca534a635487f78532449b3a8081eb765f5
Maciej Poleski <to.java.project.d8fj39saldkj28@gmail.com>
2014-01-13 14:48:19 +0100

    Add Javadoc to MetaSharer class.

--- java/src/main/java/MetaSharer.java
+++ java/src/main/java/MetaSharer.java
@@ -12,19 +12,40 @@ public class MetaSharer {
     private final FacebookSharer facebookSharer;
     private final TwitterSharer twitterSharer;
 
+    /**
+     * Tworzy obiekt który będzie wykorzystywał podane obiekty to wykonywania żądań udostępniania.
+     *
+     * @param facebookSharer Obiekt {@link FacebookSharer} który będzie służył do udostępniania na Facebook-u.
+     * @param twitterSharer  Obiekt {@link TwitterSharer} który będzie służył do udostępniania na Twitter-ze.
+     */
     public MetaSharer(FacebookSharer facebookSharer, TwitterSharer twitterSharer) {
         this.facebookSharer = facebookSharer;
         this.twitterSharer = twitterSharer;
     }
 
+    /**
+     * Tworzy instancje przy użyciu domyślnych konstruktorów klas {@link FacebookSharer} i {@link TwitterSharer}.
+     *
+     * @return Instancja stworzona przy użyciu domyślnych parametrów.
+     */
     public static MetaSharer defaultMetaSharer() {
         return new MetaSharer(new FacebookSharer(), new TwitterSharer());
     }
 
+    /**
+     * Udostępnia na Facebook-u.
+     *
+     * @param urlToObjectToShare Link który zostanie udostępniony
+     */
     public void shareOnFacebook(URL urlToObjectToShare) {
         facebookSharer.share(urlToObjectToShare);
     }
 
+    /**
+     * Udostępnia na Twitter-ze.
+     *
+     * @param urlToObjectToShare Link który zostanie udostępniony.
+     */
     public void shareOnTwitter(URL urlToObjectToShare) {
         twitterSharer.share(urlToObjectToShare);
     }
